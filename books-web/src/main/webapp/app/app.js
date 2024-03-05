f75f56c8211537694932601cd49acf9cb97e3b56
aryan chandramania <102795697+aryanchandramania@users.noreply.github.com>
2024-03-05 12:54:01 +0530

    refactor books-web/src/main/webapp/app/app.js

--- books-web/src/main/webapp/app/app.js
+++ books-web/src/main/webapp/app/app.js
@@ -1,170 +1,157 @@
 'use strict';
 
-/**
- * Sismics Books application.
- */
-var App = angular.module('books',
-    // Dependencies
-    ['ui.router', 'restangular', 'ngSanitize',
-      'colorpicker.module', 'ui.keypress', 'ui.validate',]
-  )
+var App = angular.module('books', ['ui.router', 'restangular', 'ngSanitize', 'colorpicker.module', 'ui.keypress', 'ui.validate']);
 
-/**
- * Configuring modules.
- */
-.config(function($stateProvider, $httpProvider, RestangularProvider) {
-  // Configuring UI Router
+App.config(function($stateProvider, $httpProvider, RestangularProvider) {
   $stateProvider
-  .state('main', {
-    url: '',
-    views: {
-      'page': {
-        templateUrl: 'partial/main.html',
-        controller: 'Main'
-      }
-    }
-  })
-  .state('bookaddmanual', {
-    url: '/book/addmanual',
-    views: {
-      'page': {
-        templateUrl: 'partial/book.edit.html',
-        controller: 'BookAddManual'
-      }
-    }
-  })
-  .state('book', {
-    url: '/book/:tag',
-    views: {
-      'page': {
-        templateUrl: 'partial/book.html',
-        controller: 'Book'
-      }
-    }
-  })
-  .state('bookview', {
-    url: '/book/view/:id',
-    views: {
-      'page': {
-        templateUrl: 'partial/book.view.html',
-        controller: 'BookView'
-      }
-    }
-  })
-  .state('bookedit', {
-    url: '/book/edit/:id',
-    views: {
-      'page': {
-        templateUrl: 'partial/book.edit.html',
-        controller: 'BookEdit'
-      }
-    }
-  })
-  .state('bookadd', {
-    url: '/book/add/:isbn',
-    views: {
-     'page': {
-       templateUrl: 'partial/book.add.html',
-       controller: 'BookAdd'
-     }
-    }
-  })
-  .state('login', {
-    url: '/login',
-    views: {
-      'page': {
-        templateUrl: 'partial/login.html',
-        controller: 'Login'
-      }
-    }
-  })
-  .state('tag', {
-    url: '/tag',
-    views: {
-      'page': {
-        templateUrl: 'partial/tag.html',
-        controller: 'Tag'
-      }
-    }
-  })
-  .state('settings', {
-    url: '/settings',
-    abstract: true,
-    views: {
-      'page': {
-        templateUrl: 'partial/settings.html',
-        controller: 'Settings'
-      }
-    }
-  })
-    .state('settings.default', {
+    .state('main', {
       url: '',
       views: {
-        'settings': {
-          templateUrl: 'partial/settings.default.html',
-          controller: 'SettingsDefault'
+        'page': {
+          templateUrl: 'partial/main.html',
+          controller: 'Main'
         }
       }
     })
-    .state('settings.account', {
-      url: '/account',
+    .state('bookaddmanual', {
+      url: '/book/addmanual',
       views: {
-        'settings': {
-          templateUrl: 'partial/settings.account.html',
-          controller: 'SettingsAccount'
+        'page': {
+          templateUrl: 'partial/book.edit.html',
+          controller: 'BookAddManual'
         }
       }
     })
-    .state('settings.session', {
-      url: '/session',
+    .state('book', {
+      url: '/book/:tag',
       views: {
-        'settings': {
-          templateUrl: 'partial/settings.session.html',
-          controller: 'SettingsSession'
+        'page': {
+          templateUrl: 'partial/book.html',
+          controller: 'Book'
         }
       }
     })
-    .state('settings.log', {
-      url: '/log',
+    .state('bookview', {
+      url: '/book/view/:id',
       views: {
-        'settings': {
-          templateUrl: 'partial/settings.log.html',
-          controller: 'SettingsLog'
+        'page': {
+          templateUrl: 'partial/book.view.html',
+          controller: 'BookView'
         }
       }
     })
-    .state('settings.user', {
-      url: '/user',
+    .state('bookedit', {
+      url: '/book/edit/:id',
       views: {
-        'settings': {
-          templateUrl: 'partial/settings.user.html',
-          controller: 'SettingsUser'
+        'page': {
+          templateUrl: 'partial/book.edit.html',
+          controller: 'BookEdit'
         }
       }
     })
-      .state('settings.user.edit', {
-        url: '/edit/:username',
+    .state('bookadd', {
+      url: '/book/add/:isbn',
+      views: {
+        'page': {
+          templateUrl: 'partial/book.add.html',
+          controller: 'BookAdd'
+        }
+      }
+    })
+    .state('login', {
+      url: '/login',
+      views: {
+        'page': {
+          templateUrl: 'partial/login.html',
+          controller: 'Login'
+        }
+      }
+    })
+    .state('tag', {
+      url: '/tag',
+      views: {
+        'page': {
+          templateUrl: 'partial/tag.html',
+          controller: 'Tag'
+        }
+      }
+    })
+    .state('settings', {
+      url: '/settings',
+      abstract: true,
+      views: {
+        'page': {
+          templateUrl: 'partial/settings.html',
+          controller: 'Settings'
+        }
+      }
+    })
+      .state('settings.default', {
+        url: '',
         views: {
-          'user': {
-            templateUrl: 'partial/settings.user.edit.html',
-            controller: 'SettingsUserEdit'
+          'settings': {
+            templateUrl: 'partial/settings.default.html',
+            controller: 'SettingsDefault'
           }
         }
       })
-      .state('settings.user.add', {
-        url: '/add',
+      .state('settings.account', {
+        url: '/account',
         views: {
-          'user': {
-            templateUrl: 'partial/settings.user.edit.html',
-            controller: 'SettingsUserEdit'
+          'settings': {
+            templateUrl: 'partial/settings.account.html',
+            controller: 'SettingsAccount'
           }
         }
-      });
-  
-  // Configuring Restangular
+      })
+      .state('settings.session', {
+        url: '/session',
+        views: {
+          'settings': {
+            templateUrl: 'partial/settings.session.html',
+            controller: 'SettingsSession'
+          }
+        }
+      })
+      .state('settings.log', {
+        url: '/log',
+        views: {
+          'settings': {
+            templateUrl: 'partial/settings.log.html',
+            controller: 'SettingsLog'
+          }
+        }
+      })
+      .state('settings.user', {
+        url: '/user',
+        views: {
+          'settings': {
+            templateUrl: 'partial/settings.user.html',
+            controller: 'SettingsUser'
+          }
+        }
+      })
+        .state('settings.user.edit', {
+          url: '/edit/:username',
+          views: {
+            'user': {
+              templateUrl: 'partial/settings.user.edit.html',
+              controller: 'SettingsUserEdit'
+            }
+          }
+        })
+        .state('settings.user.add', {
+          url: '/add',
+          views: {
+            'user': {
+              templateUrl: 'partial/settings.user.edit.html',
+              controller: 'SettingsUserEdit'
+            }
+          }
+        });
+      
   RestangularProvider.setBaseUrl('api');
   
-  // Configuring $http to act like jQuery.ajax
   $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
   $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
   $httpProvider.defaults.transformRequest = [function(data) {
@@ -183,7 +170,8 @@ var App = angular.module('books',
             innerObj[fullSubName] = subValue;
             query += param(innerObj) + '&';
           }
-        } else if(value instanceof Object) {
+        } 
+        else if(value instanceof Object) {
           for(subName in value) {
             subValue = value[subName];
             fullSubName = name + '[' + subName + ']';
@@ -202,12 +190,9 @@ var App = angular.module('books',
     
     return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
   }];
-})
+});
 
-/**
- * Application initialization.
- */
-.run(function($rootScope, $state, $stateParams) {
+App.run(function($rootScope, $state, $stateParams) {
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
 });
